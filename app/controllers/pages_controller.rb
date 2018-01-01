5b05b57f4cb3f678b2ce9465515bfeac243d5025
MaximKiselyow <m.kiselyow@gmail.com>
2018-01-02 00:52:28 +0200

    adaptive landing page added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def landing_page_example
   end
+
+  def adaptive_landing_page_example
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: "pages#landing_page_example"
 
   get '/landing_page_example' => 'pages#landing_page_example'
+  get '/adaptive_landing_page_example' => 'pages#adaptive_landing_page_example'
 end
